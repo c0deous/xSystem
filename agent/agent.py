eeb9d1ba2cb88d347bccdf2d9caf93be3cc1a885
Jesse Wallace <jgwallace19@me.com>
2014-03-16 19:49:35 -0500

    updated controller and agent scripts

--- agent/agent.py
+++ agent/agent.py
@@ -5,9 +5,22 @@ import commands
 import httplib
 
 print('[*] xSystem Agent Version 1.0')
+print('[*] This program was written for (OS Name)')
 
-#import plugins
-import xsystemplugins
+#configuration
+
+portnumber = 1237  #port that the program runs on. leave at 1237 if unsure.
+
+debugapi = False   #useful if you are writing your own commands and functions
+
+hostip = '0.0.0.0' #This allows you to make the server local only or make it accessible by a controller
+				   #Keep on 0.0.0.0 to make it accessible by a controller
+				   #Change to localhost if you want it to be local only
+
+
+
+
+
 
 
 #default commands
@@ -16,7 +29,7 @@ def harddisks():
     cmd = commands.getstatusoutput('df -h')
     return cmd
 
-@route('/info/')
+@route('/info')
 def info():
     cmd = commands.getstatusoutput('uptime')
     return cmd
@@ -31,4 +44,10 @@ def ifconfig2(interface=""):
     cmd = commands.getstatusoutput('ifconfig ' + interface)
     return cmd
 
-run(host='0.0.0.0', port=1237, debug=False)
+#Custom functions and commands go below this line
+
+
+
+
+run(host=hostip, port=portnumber, debug=debugapi)
+print('[*] Quitting...')
--- controller/controller.py
+++ controller/controller.py
@@ -4,29 +4,50 @@ import os
 import commands
 import httplib
 
-print ('[*] xSystem Controller Version 1.0')
+print('[*] xSystem Controller Version 1.0')
+print('[*] This program was written for (OS Name)')
+#configuration
 
+portnumber = 1238  #port that the program runs on. leave at 1237 if unsure.
+
+debugapi = False   #useful if you are writing your own commands and functions
+
+hostip = '0.0.0.0' #This allows you to make the server local only or make it accessible by a controller
+				   #Keep on 0.0.0.0 to make it accessible by a controller
+				   #Change to localhost if you want it to be local only
+
+#hostnames ----- put hostnames and their ip addresses here
+hostNames = {'mac-pro' : 'jessewallacemacpro.local', 'imac' : 'imac.local'}
+
+
+
+
+#default commands
 @route('/<hostname>/')
 def hostnameinfo(hostname="localhost"):
     connection = httplib.HTTPConnection(hostname + ':1237')
-    connection.request('GET', '/info/')
+    connection.request('GET', '/info')
     r1 = connection.getresponse()
     hostnameinfo = r1.read()
     return hostnameinfo
     connection.close()
     
 @route('/<hostname>/<command>/<command1>')
-def runhostcommand(hostname="localhost", command="/info/"):
+def runhostcommand(hostname="localhost", command="/info", command1=""):
     connection = httplib.HTTPConnection(hostname + ':1237')
-    
-    if command2 == "":
-        connection.request('GET', '/' + command + '/')
-    else:
-        connection.request('GET', '/' + command + '/' + command1)
-        
+    connection.request('GET', '/' + command + '/' + command1)   
     r1 = connection.getresponse()
     agentreturn = r1.read()
     return agentreturn
     connection.close()
 
-run(host='0.0.0.0', port=1238, debug=False)
+@route('/<hostname>/<command>')
+def runhostcommand(hostname="localhost", command="/info"):
+	connection = httplib.HTTPConnection(hostname + ':1237')
+	connection.request('GET', '/' + command)   
+	r1 = connection.getresponse()
+	agentreturn = r1.read()
+	return agentreturn
+	connection.close()
+	
+run(host=hostip, port=portnumber, debug=debugapi)
