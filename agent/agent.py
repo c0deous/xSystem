3c481b5f1f2dc4bf7984fd425168f438f55b41c3
Jesse Wallace <jgwallace19@me.com>
2014-03-18 15:23:38 -0500

    Continued updating scripts

--- agent/agent.py
+++ agent/agent.py
@@ -1,33 +1,57 @@
 #!/usr/bin/env python
-from bottle import route, run
+from bottle import route, run, Bottle, ServerAdapter
 import os
 import commands
 import httplib
 
+
 print('[*] xSystem Agent Version 1.0')
+if os.path.isfile('installerdone.note') == False:
+    print('[-] You need to run "installer.py" in the agent folder first.')
+    exit()
+else:
+    pass
 print('[*] This program was written for (OS Name)')
 
-#configuration
+
+# --------- Configuration -------------
 
 portnumber = 1237  #port that the program runs on. leave at 1237 if unsure.
 
 debugapi = False   #useful if you are writing your own commands and functions
 
 hostip = '0.0.0.0' #This allows you to make the server local only or make it accessible by a controller
-				   #Keep on 0.0.0.0 to make it accessible by a controller
-				   #Change to localhost if you want it to be local only
+                   #Keep on 0.0.0.0 to make it accessible by a controller
+                   #Change to localhost if you want it to be local only
+
+saltfile = 'salt.salt' #put path to your "salt" file here.  This should be generated when you run the install script.
+                       #must be exactly 8 digits
+
+tempsalt = '12345678'
+# ------ Config Setup Code ---------
 
 
 
+salt = tempsalt
+#-------- Encryption -------------
+from pyDes import *
+
+def encrypt(data, password,):
+    k = des(password, CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5,)
+    d = k.encrypt(data,)
+    return d
+def decrypt(data, password,):
+    k = des(password, CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5,)
+    d = k.decrypt(data,)
+    return d
 
 
-
-
-#default commands
+#--------- Default Commands ----------
 @route('/harddisks')
 def harddisks():
     cmd = commands.getstatusoutput('df -h')
-    return cmd
+    cmdEnc = encrypt(cmd, salt)
+    return cmdEnc
 
 @route('/info')
 def info():
@@ -46,8 +70,15 @@ def ifconfig2(interface=""):
 
 #Custom functions and commands go below this line
 
+#Note about custom commands and functions:
+#currently xsystem only supports a maximum of two arguments in a request
+#so you cannot currently do something like @route('/somearg/somearg/somearg')
+#you could do something like @route('/somearg/somearg')
+#DO NOT PUT A FORWARD SLASH AT THE END OF A REQUEST!!! It will cause all sorts of errors and it is generally easier to sort through if you leave a / off of the end.
 
 
-
+# -------- Server Initiation ----------
 run(host=hostip, port=portnumber, debug=debugapi)
+
+# ---------- Cleanup Stuff on quit -----------
 print('[*] Quitting...')
--- controller/controller.py
+++ controller/controller.py
@@ -1,53 +1,155 @@
 #!/usr/bin/env python
-from bottle import route, run
+from bottle import route, run, Bottle, ServerAdapter
 import os
 import commands
 import httplib
 
 print('[*] xSystem Controller Version 1.0')
+if os.path.isfile('installerdone.note') == False:
+    print('[-] You need to run "installer.py" in the agent folder first.')
+    exit()
+else:
+    pass
 print('[*] This program was written for (OS Name)')
 #configuration
 
-portnumber = 1238  #port that the program runs on. leave at 1237 if unsure.
+portnumber = 1238  #port that the program runs on. leave at 1238 if unsure.
 
-debugapi = False   #useful if you are writing your own commands and functions
 
 hostip = '0.0.0.0' #This allows you to make the server local only or make it accessible by a controller
-				   #Keep on 0.0.0.0 to make it accessible by a controller
-				   #Change to localhost if you want it to be local only
+                   #Keep on 0.0.0.0 to make it accessible by a controller
+                   #Change to localhost if you want it to be local only
 
-#hostnames ----- put hostnames and their ip addresses here
-hostNames = {'mac-pro' : 'jessewallacemacpro.local', 'imac' : 'imac.local'}
+authtype = 'controller' #Allows you to choose what kind of authentication to use
+                        #Authentication Types: controller, none
+                        #Controller [Normal Security] lets you have a single password to use to access any agent computer
+                        #None [No Security] has no password for anything (not reccomended)
+saltfile = 'salt.salt'
 
+salt = '12345678'
 
+#-------hostnames-------
+#put hostnames and their ip addresses here
+#Format: 'friendlyname' : 'ip',
+hostNames = {
 
+'localhost' : 'localhost', 
+'mac-pro' : '192.168.0.108', 
+'imac' : '192.168.0.106',
+'osxserver' : '192.168.0.100',
+'router' : '192.168.0.1'
 
-#default commands
+}
+
+#----- Config Setup Code -----
+if authtype == 'controller':
+    def xsystemAuth(password):
+        if password == 'lamepass':
+            print(True)
+        else:
+            print(False)
+if authtype == 'none':
+    def xsystemAuth():
+        print(True)
+
+#------ SSL Stuff ------
+class SSLWSGIRefServer(ServerAdapter):
+    def run(self, handler):
+        from wsgiref.simple_server import make_server, WSGIRequestHandler
+        import ssl
+        if self.quiet:
+            class QuietHandler(WSGIRequestHandler):
+                def log_request(*args, **kw): pass
+            self.options['handler_class'] = QuietHandler
+        srv = make_server(self.host, self.port, handler, **self.options)
+        srv.socket = ssl.wrap_socket (
+            srv.socket,
+            certfile='server.pem',  # path to certificate
+            server_side=True)
+        srv.serve_forever()
+
+# ------- Encryption ---------
+from pyDes import *
+
+def encrypt(data, password):
+    k = des(password, CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
+    d = k.encrypt(data)
+    return d
+def decrypt(data, password):
+    k = des(password, CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
+    d = k.decrypt(data)
+    return d
+
+#--------- Default Commands ------------
 @route('/<hostname>/')
 def hostnameinfo(hostname="localhost"):
-    connection = httplib.HTTPConnection(hostname + ':1237')
-    connection.request('GET', '/info')
-    r1 = connection.getresponse()
-    hostnameinfo = r1.read()
-    return hostnameinfo
-    connection.close()
+    try:
+        hostnameip = hostNames[hostname]
+    except KeyError:
+        return 'Hostname "' + hostname + '" is not in hostname list.'
+    except:
+        return 'Unexpected error with hostnames list'
+    else:
+        try:
+            connection = httplib.HTTPConnection(hostnameip + ':1237')
+            connection.request('GET', '/info')
+        except:
+            return 'unexpected internal server error 500'
+        else:
+            r1 = connection.getresponse()
+            hostnameinfo = r1.read()
+            hostnameinfoDec = decrypt(hostnameinfo, salt)
+            return hostnameinfoDec
+            connection.close()
     
 @route('/<hostname>/<command>/<command1>')
 def runhostcommand(hostname="localhost", command="/info", command1=""):
-    connection = httplib.HTTPConnection(hostname + ':1237')
-    connection.request('GET', '/' + command + '/' + command1)   
-    r1 = connection.getresponse()
-    agentreturn = r1.read()
-    return agentreturn
-    connection.close()
+    try:
+        hostnameip = hostNames[hostname]
+    except KeyError:
+        return 'Hostname "' + hostname + '" is not in hostname list.'
+    except:
+        return 'Unexpected error with hostnames list'
+    else:
+        try:
+            connection = httplib.HTTPConnection(hostnameip + ':1237')
+            connection.request('GET', '/' + command + '/' + command1)
+        except:
+            return 'Unexpected internal server error 500'
+        else:
+            r1 = connection.getresponse()
+            agentreturn = r1.read()
+            agentreturnDec = decrypt(agentreturn, salt)
+            return agentreturnDec
+            connection.close()
 
 @route('/<hostname>/<command>')
 def runhostcommand(hostname="localhost", command="/info"):
-	connection = httplib.HTTPConnection(hostname + ':1237')
-	connection.request('GET', '/' + command)   
-	r1 = connection.getresponse()
-	agentreturn = r1.read()
-	return agentreturn
-	connection.close()
-	
-run(host=hostip, port=portnumber, debug=debugapi)
+    try:
+        hostnameip = hostNames[hostname]
+    except KeyError:
+        return 'Hostname "' + hostname + '" is not in hostname list.'
+    except:
+        return 'Unexpected error with hostnames list'
+    else:
+        try:
+            connection = httplib.HTTPConnection(hostnameip + ':1237')
+            connection.request('GET', '/' + command)
+        except:
+            return 'Unexpected internal server error 500'
+        else:
+            r1 = connection.getresponse()
+            agentreturn = r1.read()
+            agentreturnDec = decrypt(agentreturn, salt)
+            return agentreturnDec
+            connection.close()
+#---------- Custom Commands and Functions -------------
+#DO NOT PUT A FORWARD SLASH AT THE END OF A REQUEST!!! It will cause all sorts of errors and it is generally easier to sort through if you leave a / off of the end.
+
+
+#------ Server Initiation ------    
+srv = SSLWSGIRefServer(host=hostip, port=portnumber)
+run(server=srv)
+
+#------- Cleanup Stuff on quit ------
+print('[*] Quitting...')
